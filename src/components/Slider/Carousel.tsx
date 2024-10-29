82dbeaec38ce64e1c4dd7030693710bfa346e5bd
Vinay Pallegar <vp@ipalmedia.com>
2024-10-29 12:33:10 -0700

    add carousels to homepage.

--- src/components/Slider/Carousel.tsx
+++ src/components/Slider/Carousel.tsx
@@ -45,16 +45,27 @@ export default function Carousel({ children }: CarouselProps) {
         slidesToShow: 4,
         slidesToScroll: 1,
         adaptiveHeight: false,
+        adaptiveWidth: false,
         prevArrow: <PrevArrow/>,
         nextArrow: <NextArrow/>,
+        centerPadding: '20px',
         appendArrows: '.section-arrows',
-        breakpoints: [
+        responsive: [
             {
-                breakpoint: 600,
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 550,
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 1,
-                    initialSlide: 2,
+                    dots: true,
+                    arrows: false,
                 },
             },
             {
@@ -62,6 +73,8 @@ export default function Carousel({ children }: CarouselProps) {
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1,
+                    dots: true,
+                    arrows: false,
                 },
             },
         ],
